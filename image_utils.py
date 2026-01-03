142798a5649e97854c949ba6f0682a11345e6631
214259392 <tiferetw@gmail.com>
2026-01-03 15:29:48 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,27 +1,30 @@
-from PIL import Image
 import numpy as np
+from PIL import Image
 from scipy.signal import convolve2d
 
-def load_image(path):
+def load_image(file_path):
+    # טעינה והמרה למערך
     img = Image.open(file_path)
     return np.array(img)
+
+def edge_detection(image_array):
+    # 1. המרה לגווני אפור (ממוצע ערוצים)
+    gray_image = np.mean(image_array, axis=2)
     
-def edge_detection(image):
-   gray_image = np.mean(image_array, axis=2)
-    
+    # 2. פילטרים לפי הדרישות
     filter_v = np.array([[1, 1, 1],
                          [0, 0, 0],
                          [-1, -1, -1]])
-
+    
     filter_h = np.array([[1, 0, -1],
                          [1, 0, -1],
                          [1, 0, -1]])
     
+    # 3. קונבולוציה עם zero padding
     edgeX = convolve2d(gray_image, filter_h, mode='same')
     edgeY = convolve2d(gray_image, filter_v, mode='same')
     
-    # $$edgeMAG = \sqrt{edgeX^2 + edgeY^2}$$
+    # 4. חישוב Magnitude
     edgeMAG = np.sqrt(np.square(edgeX) + np.square(edgeY))
     
     return edgeMAG
-
